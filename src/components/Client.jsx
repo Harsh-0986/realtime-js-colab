0a655e3ba79461438ed8820d8742cdad36463c72
Harsh-0986 <harsh.r.shah2102@gmail.com>
2022-03-26 16:42:51 +0530

    Created editor sidebar

---
+++ src/components/Client.jsx
@@ -0,0 +1,12 @@
+import Avatar from "react-avatar";
+
+const Client = ({ userName }) => {
+	return (
+		<div className="client">
+			<Avatar name={userName} size={50} round="14px" />
+			<span className="userName">{userName}</span>
+		</div>
+	);
+};
+
+export default Client;
--- src/pages/EditorPage.jsx
+++ src/pages/EditorPage.jsx
@@ -1,5 +1,42 @@
+import { useState } from "react";
+import Client from "../components/Client";
+import Editor from "../components/Editor";
+
 const EditorPage = () => {
-	return <div>Editor</div>;
+	const [clients, setClients] = useState([
+		{ socketId: 1, userName: "Harsh Shah" },
+		{ socketId: 2, userName: "Rohit Shah" },
+	]);
+
+	return (
+		<div className="mainWrap">
+			<div className="aside">
+				<div className="asideInner">
+					<div className="logo">
+						<img
+							src="/code-sync.png"
+							alt="logo"
+							className="logoImage"
+						/>
+					</div>
+					<h3>Connected</h3>
+					<div className="clientsList">
+						{clients.map((client) => (
+							<Client
+								userName={client.userName}
+								key={client.socketId}
+							/>
+						))}
+					</div>
+				</div>
+				<button className="btn copyBtn">Copy Room Id</button>
+				<button className="btn leaveBtn">Leave</button>
+			</div>
+			<div className="editorWrap">
+				<Editor />
+			</div>
+		</div>
+	);
 };
 
 export default EditorPage;
