63409e5836b7b3f52e120e7e1fcde816b1cfbc47
Harsh-0986 <harsh.r.shah2102@gmail.com>
2022-03-26 17:08:50 +0530

    Added codemirror editor

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,5 +1,27 @@
+import { useEffect } from "react";
+import Codemirror from "codemirror";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/theme/material-ocean.css";
+import "codemirror/addon/edit/closetag";
+import "codemirror/addon/edit/closebrackets";
+import "codemirror/lib/codemirror.css";
+
 const Editor = () => {
-	return <div>Editor</div>;
+	useEffect(() => {
+		const init = async () => {
+			Codemirror.fromTextArea(document.getElementById("realtimeEditor"), {
+				mode: { name: "javascript", json: true },
+				theme: "material-ocean",
+				autoCloseTags: true,
+				autoCloseBrackets: true,
+				lineNumbers: true,
+			});
+		};
+
+		init();
+	}, []);
+
+	return <textarea id="realtimeEditor"></textarea>;
 };
 
 export default Editor;
