ef044aa13be0f8f59e21cb4ed5b81c1312a7a07a
Harsh-0986 <harsh.r.shah2102@gmail.com>
2022-03-26 15:59:39 +0530

    Added the toast

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Toaster } from "react-hot-toast";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import EditorPage from "./pages/EditorPage";
@@ -6,6 +7,17 @@ import Home from "./pages/Home";
 function App() {
 	return (
 		<>
+			<div>
+				<Toaster
+					position="top-center"
+					toastOptions={{
+						success: {
+							theme: { primary: "#4aed88" },
+						},
+					}}
+				></Toaster>
+			</div>
+
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<Home />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,20 @@
+import { v4 as uuidV4 } from "uuid";
+import { useState } from "react";
+import toast from "react-hot-toast";
+
 const Home = () => {
+	const [id, setId] = useState();
+	const [userName, setUserName] = useState();
+
+	const createNewRoom = (e) => {
+		e.preventDefault();
+
+		setId(uuidV4());
+
+		//Toast
+		toast.success("Created a new room");
+	};
+
 	return (
 		<div className="homePageWrapper">
 			<div className="formWrapper">
@@ -13,15 +29,23 @@ const Home = () => {
 						type="text"
 						placeholder="ROOM ID"
 						className="inputBox"
+						value={id}
+						onChange={(e) => setId(e.target.value)}
 					/>
 					<input
 						type="text"
 						placeholder="USERNAME"
 						className="inputBox"
+						onChange={(e) => setUserName(e.target.value)}
+						value={userName}
 					/>
 					<button className="btn joinBtn">Join</button>
 					<span className="createInfo">
-						<a href="" className="createNewBtn">
+						<a
+							onClick={createNewRoom}
+							href="/"
+							className="createNewBtn"
+						>
 							Create new room
 						</a>
 					</span>
