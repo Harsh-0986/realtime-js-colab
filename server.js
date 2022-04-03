61d01383f30fdd0b5c1d6219610f5aa13b0ece0f
Harsh-0986 <harsh.r.shah2102@gmail.com>
2022-04-03 16:36:50 +0530

    Syncing the code of different users

--- server.js
+++ server.js
@@ -38,6 +38,10 @@ io.on("connection", (socket) => {
 		});
 	});
 
+	socket.on(ACTIONS.CODE_CHANGE, ({ roomId, code }) => {
+		socket.in(roomId).emit(ACTIONS.CODE_CHANGE, { code });
+	});
+
 	socket.on("disconnecting", () => {
 		const rooms = [...socket.rooms];
 
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,25 +1,52 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import Codemirror from "codemirror";
 import "codemirror/mode/javascript/javascript";
 import "codemirror/theme/material-ocean.css";
 import "codemirror/addon/edit/closetag";
 import "codemirror/addon/edit/closebrackets";
 import "codemirror/lib/codemirror.css";
+import ACTIONS from "../actions";
 
-const Editor = () => {
+const Editor = ({ socketRef, roomId }) => {
+	const editorRef = useRef();
+
 	useEffect(() => {
 		const init = async () => {
-			Codemirror.fromTextArea(document.getElementById("realtimeEditor"), {
-				mode: { name: "javascript", json: true },
-				theme: "material-ocean",
-				autoCloseTags: true,
-				autoCloseBrackets: true,
-				lineNumbers: true,
+			editorRef.current = Codemirror.fromTextArea(
+				document.getElementById("realtimeEditor"),
+				{
+					mode: { name: "javascript", json: true },
+					theme: "material-ocean",
+					autoCloseTags: true,
+					autoCloseBrackets: true,
+					lineNumbers: true,
+				}
+			);
+
+			editorRef.current.on("change", (instance, changes) => {
+				const { origin } = changes;
+				const code = instance.getValue();
+				if (origin !== "setValue") {
+					socketRef.current.emit(ACTIONS.CODE_CHANGE, {
+						roomId,
+						code,
+					});
+				}
 			});
 		};
 
 		init();
 	}, []);
+
+	useEffect(() => {
+		if (socketRef.current) {
+			socketRef.current.on(ACTIONS.CODE_CHANGE, ({ code }) => {
+				if (code !== null) {
+					editorRef.current.setValue(code);
+				}
+			});
+		}
+	}, [socketRef.current]);
 
 	return <textarea id="realtimeEditor"></textarea>;
 };
